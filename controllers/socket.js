256bb4a51983a531bb14a5354fffbd519c545962
Mohsin Zafar <heykhattak@gmail.com>
2024-09-29 11:38:27 +0500

    webrtc half work done

--- controllers/socket.js
+++ controllers/socket.js
@@ -20,8 +20,8 @@ const getUser = asyncHandler(async (userId) => {
   return await Socket.findOne({ userId });
 });
 
-const activeStreams = {};
 const activeUsers = {};
+const activeStreams = [];
 
 const handleSocketConnection = (io) => {
   io.on("connection", (socket) => {
@@ -49,66 +49,29 @@ const handleSocketConnection = (io) => {
       }
     });
 
-    // Handle offer from broadcaster
-    socket.on("offer", (data) => {
-      const { offer, broadcasterId } = data;
-
-      activeStreams[broadcasterId] = {
-        offer,
-        streamerSocketId: socket.id,
-        broadcasterId: broadcasterId,
-      };
-
-      io.emit("streams-available", activeStreams);
-      console.log(`Offer stored for broadcaster: ${broadcasterId}`);
-    });
-
-    // When a viewer wants to watch a stream
-    socket.on("watch-stream", (broadcasterId) => {
-      if (activeStreams[broadcasterId]) {
-        const { offer, streamerSocketId } = activeStreams[broadcasterId];
-        socket.join(broadcasterId);
-        socket.emit("offer", { offer, broadcasterId: streamerSocketId });
-        console.log(
-          `Offer sent to viewer: ${socket.id} for broadcaster: ${broadcasterId}`
-        );
-      } else {
-        socket.emit("stream-not-found", { message: "Stream not available" });
+    socket.on("signal", (data) => {
+      if (data.role === "creator") {
+        console.log("Signal Received: ", data);
+        activeStreams.push({
+          userId: data.userId,
+          description: data.description,
+        });
+        socket.broadcast.emit("streams", activeStreams);
       }
-    });
-
-    // When viewer sends an answer to the broadcaster
-    socket.on("answer", (data) => {
-      const { answer, broadcasterId } = data;
-      socket.to(broadcasterId).emit("answer", { answer, viewerId: socket.id });
-      console.log(`Answer sent to broadcaster: ${broadcasterId}`);
-    });
-
-    // Handle ICE candidates
-    socket.on("ice-candidate", (data) => {
-      const { candidate, targetId } = data;
-      if (targetId) {
-        socket.to(targetId).emit("ice-candidate", candidate);
-        console.log(`ICE candidate sent to: ${targetId}`);
-      } else {
-        console.error("ICE candidate target ID is missing or undefined");
-      }
+      socket.broadcast.emit("signal", data);
     });
 
     socket.on("disconnect", async () => {
       await removeUser(socket.id);
 
-      // Find the userId associated with the disconnected socket
       const userId = Object.keys(activeUsers).find(
         (key) => activeUsers[key].socketId === socket.id
       );
 
       if (userId) {
-        // Remove the user from the activeUsers object
         delete activeUsers[userId];
         console.log(`User disconnected: ${userId}`);
 
-        // Emit the updated active users list
         io.emit("active-users", Object.values(activeUsers));
       }
     });
