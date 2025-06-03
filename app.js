8e7ae686c79b273d18b50d50ce5a1cbe7c409638
saadasif04 <asifnawazmalik75@gmai.com@example.com>
2025-06-03 12:35:24 +0500

    Fixed image problem

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/api/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(
   session({
     secret: process.env.SESSION_SECRET_KEY,
